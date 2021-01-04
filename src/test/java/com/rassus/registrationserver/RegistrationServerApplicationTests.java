2ddd3da7f750362fef5d81a3056f2476ca23ff2d
Bartol <bartol.bilic1@gmail.com>
2021-01-04 19:04:32 +0100

    initial commit

---
+++ src/test/java/com/rassus/registrationserver/RegistrationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rassus.registrationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
